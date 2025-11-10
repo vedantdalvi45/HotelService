1d2f1a2e91f979a95664cd47c99343cf31ffa588
vedantdalvi45 <vedalvi16@gmail.com>
2025-11-10 19:46:15 +0530

    Fix : Added Unique Domain Of name Column

--- src/main/java/com/ved/hotel/HotelService/model/Hotel.java
+++ src/main/java/com/ved/hotel/HotelService/model/Hotel.java
@@ -1,5 +1,6 @@
 package com.ved.hotel.HotelService.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -17,6 +18,7 @@ import lombok.Setter;
 public class Hotel {
     @Id
     private String id;
+    @Column(unique = true)
     private String name;
     private  String location;
     private String about;
